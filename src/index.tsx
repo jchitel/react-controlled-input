8e38f7d5bb8d1a64f2788109d3f043bed38d6c21
Jake Chitel <jchitel@gmail.com>
2018-08-28 00:03:24 -0500

    initial files and config

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export interface IControlledInputProps {
+    //
+}
+
+export default class ControlledInput extends React.PureComponent<IControlledInputProps> {
+    //
+}
